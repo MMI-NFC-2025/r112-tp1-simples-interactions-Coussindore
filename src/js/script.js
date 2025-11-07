132378ace28c3466c0a8817d7dc80e8ca248f006
Coussindore <125923831+Coussindore@users.noreply.github.com>
2025-11-07 09:27:51 +0100

    Ajout d'event pour mettre en rouge les H2

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,8 @@ aMettreEnRouge.style.color="red";
 
 const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click")
 enRougeSuiteAClick.addEventListener ("click", (evt)=>{enRougeSuiteAClick.style.color = "red"});
+
+const collh2 = document.querySelectorAll ("h2");
+collh2.forEach ((elm)=>{
+    elm.addEventListener ("click", (evt)=>{evt.target.style.color = "red"});
+});
